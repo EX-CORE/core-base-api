92b125cf539ffe80cc01a8101f9d627cb0decfa5
jieunhong <hong.bbubbu@gmail.com>
2024-03-14 21:06:35 +0900

    [#13] 리뷰어 프로젝트 테이블 생성

---
+++ src/main/kotlin/com/core/base/corebase/domain/review/ReviewerProject.kt
@@ -0,0 +1,11 @@
+package com.core.base.corebase.domain.review
+
+import java.util.*
+
+class ReviewerProject(
+    val projectId: UUID,
+    val reviewerIds : UUID,
+    val reviewId: UUID,
+) {
+    
+}
