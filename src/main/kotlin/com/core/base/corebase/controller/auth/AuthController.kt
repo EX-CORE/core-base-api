be624e51abf7b9149f383ac000513ab676a9336b
jieunhong <hong.bbubbu@gmail.com>
2024-04-12 19:34:29 +0900

    [UPDATE] Controller Swagger 추가

--- src/main/kotlin/com/core/base/corebase/controller/auth/AuthController.kt
+++ src/main/kotlin/com/core/base/corebase/controller/auth/AuthController.kt
@@ -4,12 +4,10 @@ import com.core.base.corebase.client.dto.AuthDto
 import com.core.base.corebase.common.exception.code.ServerType
 import com.core.base.corebase.service.auth.AuthService
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletResponse
 import org.apache.naming.ResourceRef.AUTH
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "로그인 API")
 @Controller
@@ -18,13 +16,16 @@ class AuthController(
 ) {
 
     @GetMapping("${AUTH}/code")
-    fun moveUserGoogleCode(@RequestParam("type") type: ServerType): String =
-        "redirect:${authService.getUserGoogleCode(type)}"
+    fun moveUserGoogleCode(@RequestParam("type") type: ServerType, response: HttpServletResponse) =
+        response.sendRedirect("redirect:${authService.getUserGoogleCode(type)}")
 
+
+    @ResponseBody
     @PostMapping("${AUTH}/login")
     fun login(@RequestParam("code") code: String, @RequestParam("type") type: ServerType): AuthDto.LoginRes =
         authService.login(code, type)
 
+    @ResponseBody
     @PostMapping("${AUTH}/token-refresh")
     fun tokenRefresh(@RequestBody req: AuthDto.TokenRefreshReq): AuthDto.TokenRefreshRes =
         authService.tokenRefresh(req)
