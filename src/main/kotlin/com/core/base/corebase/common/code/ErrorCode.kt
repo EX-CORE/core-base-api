ce046a00af27831d110a3841223faa288f9d3ca2
dohyeon5626 <kodohyeon71@gmail.com>
2024-08-17 11:17:16 +0900

    [ADD] exception handler 설정 추가

--- src/main/kotlin/com/core/base/corebase/common/code/ErrorCode.kt
+++ src/main/kotlin/com/core/base/corebase/common/code/ErrorCode.kt
@@ -8,8 +8,9 @@ enum class ErrorCode(
     val detail: String
 ) {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid Token", "(token: {VALUE})"),
-    ORGANIZATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회사가 존재하지 않습니다. ", "(id: {VALUE})"),
-    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리뷰가 존재하지 않습니다. ", "(id: {VALUE})"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다. ", "(uid: {VALUE})"),
-    REVIEWER_NOT_ALLOWED(HttpStatus.FORBIDDEN, "해당 리뷰를 작성할 권한이 없습니다. ", "(id: {VALUE})")
+    ORGANIZATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회사가 존재하지 않습니다.", "(id: {VALUE})"),
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리뷰가 존재하지 않습니다.", "(id: {VALUE})"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다.", "(uid: {VALUE})"),
+    REVIEWER_NOT_ALLOWED(HttpStatus.FORBIDDEN, "해당 리뷰를 작성할 권한이 없습니다.", "(id: {VALUE})"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류가 발생하였습니다.", "(message: {VALUE})");
 }
