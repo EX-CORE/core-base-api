cee2a206ea429bddcfe25fd74592c44e2d7aa640
dohyeon5626 <kodohyeon71@gmail.com>
2024-08-17 13:22:59 +0900

    [UPDATE] 불필요한 api 삭제

--- src/main/kotlin/com/core/base/corebase/controller/user/UserController.kt
+++ src/main/kotlin/com/core/base/corebase/controller/user/UserController.kt
@@ -2,12 +2,11 @@ package com.core.base.corebase.controller.user
 
 import com.core.base.corebase.config.AuthenticationFacade
 import com.core.base.corebase.controller.user.dto.UserOrganizationRes
-import com.core.base.corebase.controller.user.dto.UserReq
-import com.core.base.corebase.domain.user.User
 import com.core.base.corebase.service.user.UserService
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 
 @Tag(name = "유저 API")
@@ -17,10 +16,6 @@ class UserController(
     private val userService: UserService,
     private val authenticationFacade: AuthenticationFacade
 ) {
-
-    @PostMapping("organization/{organizationId}")
-    fun save(@PathVariable organizationId: UUID,
-             @RequestBody userReqList: List<UserReq>) : List<User> = userService.save(organizationId, userReqList)
 
     @GetMapping("/organizations")
     fun getUserOrganization(): UserOrganizationRes = userService.getUserOrganization(authenticationFacade.uid)
