02a92b092c0dbf09904d61419b9694c3ea1debb4
jieunhong <hong.bbubbu@gmail.com>
2024-12-30 20:25:41 +0900

    [ADD] objectMapper 빈 등록, auditing 활성화 추가

--- src/main/kotlin/com/core/base/corebase/CoreBaseApplication.kt
+++ src/main/kotlin/com/core/base/corebase/CoreBaseApplication.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.cloud.openfeign.FeignAutoConfiguration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
@@ -17,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 @ImportAutoConfiguration(FeignAutoConfiguration::class)
 @EntityScan(basePackages = ["com.core.base.corebase"])
 @EnableJpaRepositories("com.core.base.corebase")
+@EnableJpaAuditing
 class CoreBaseApplication
 
 fun main(args: Array<String>) {
