eacbc32c68476f6603ff9bc139954cd100228f28
jieunhong <hong.bbubbu@gmail.com>
2025-01-15 18:51:37 +0900

    [fix]fix

--- src/main/kotlin/com/core/base/corebase/config/CommonConfig.kt
+++ src/main/kotlin/com/core/base/corebase/config/CommonConfig.kt
@@ -17,31 +17,31 @@ import java.time.format.DateTimeFormatter
 
 @Configuration
 public class CommonConfig {
-    @Bean
-    fun objectMapper(): ObjectMapper {
-        val mapper = ObjectMapper()
-
-        // JavaTimeModule 설정
-        val module = JavaTimeModule()
-
-        // LocalDate 처리
-        module.addDeserializer(LocalDate::class.java, LocalDateDeserializer(DateTimeFormatter.ISO_DATE))
-        module.addSerializer(LocalDate::class.java, LocalDateSerializer(DateTimeFormatter.ISO_DATE))
-
-        // LocalDateTime 처리
-        module.addDeserializer(LocalDateTime::class.java, LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME))
-        module.addSerializer(LocalDateTime::class.java, LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME))
-
-        // 날짜 형식 설정
-        mapper.setDateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"))
-
-        // 추가적인 모듈 등록
-        mapper.registerModule(ParameterNamesModule())
-        mapper.registerModule(Jdk8Module())
-
-        // JavaTimeModule 등록
-        mapper.registerModule(module)
-
-        return mapper
-    }
+//    @Bean
+//    fun objectMapper(): ObjectMapper {
+//        val mapper = ObjectMapper()
+//
+//        // JavaTimeModule 설정
+//        val module = JavaTimeModule()
+//
+//        // LocalDate 처리
+//        module.addDeserializer(LocalDate::class.java, LocalDateDeserializer(DateTimeFormatter.ISO_DATE))
+//        module.addSerializer(LocalDate::class.java, LocalDateSerializer(DateTimeFormatter.ISO_DATE))
+//
+//        // LocalDateTime 처리
+//        module.addDeserializer(LocalDateTime::class.java, LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME))
+//        module.addSerializer(LocalDateTime::class.java, LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME))
+//
+//        // 날짜 형식 설정
+//        mapper.setDateFormat(SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS"))
+//
+//        // 추가적인 모듈 등록
+//        mapper.registerModule(ParameterNamesModule())
+//        mapper.registerModule(Jdk8Module())
+//
+//        // JavaTimeModule 등록
+//        mapper.registerModule(module)
+//
+//        return mapper
+//    }
 }
